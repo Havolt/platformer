4c8911bb35ed9b65f177444152712426b3153940
Mark Fitzpatrick <markfitz815@gmail.com>
2018-04-19 21:16:07 +0100

    added array for engine to call functions from

--- scripts.js
+++ scripts.js
@@ -6,6 +6,11 @@ let ctxData = {
     height: 540
 }
 
+let engineData = {
+    gameStarted : false,
+    activeFuncs : []
+}
+
 
 function defineCanvasStyle(canvasId, width, height){
     canvasId.setAttribute('width', width + 'px');
@@ -13,16 +18,21 @@ function defineCanvasStyle(canvasId, width, height){
     canvasId.style.border = "1px solid black";
 }
 
+function testFunc1(){console.log('a')};
+function testFunc2(){console.log('b')};
+
 function gameEngine(){
-    
+    for(let i = 0; i < engineData.activeFuncs.length; i++){
+        engineData.activeFuncs[i]();
+    }
     //setTimeout(gameEngine, 20);
 }
 
 function drawMenu(){
     //Create linear gradient
     let menuData ={
-        buttonWidth : 210,
-        buttonHeight : 75
+        buttonWidth : 254,
+        buttonHeight : 60
     }
     let grd = ctx.createLinearGradient(0,0, 0, ctxData.height);
     grd.addColorStop(0, '#3a3a3a');
@@ -31,20 +41,29 @@ function drawMenu(){
     ctx.fillRect(0,0, ctxData.width, ctxData.height);
     //Create button
     ctx.strokeStyle='#fefefe';
-    ctx.strokeRect((ctxData.width /2)-(menuData.buttonWidth /2),(ctxData.height /2)-(menuData.buttonHeight /2) - 40,menuData.buttonWidth, menuData.buttonHeight)
-    ctx.fillStyle="#417dc6";
-    ctx.fillRect((ctxData.width /2)-(menuData.buttonWidth /2),(ctxData.height /2)-(menuData.buttonHeight /2) - 40,menuData.buttonWidth, menuData.buttonHeight);
+    ctx.strokeRect((ctxData.width /2)-(menuData.buttonWidth /2),(ctxData.height /2)-(menuData.buttonHeight /2) - 20,menuData.buttonWidth, menuData.buttonHeight)
+    ctx.fillStyle="#2b67c6";
+    ctx.fillRect((ctxData.width /2)-(menuData.buttonWidth /2),(ctxData.height /2)-(menuData.buttonHeight /2) - 20,menuData.buttonWidth, menuData.buttonHeight);
     ctx.fillStyle="#fefefe";
-    ctx.font="56px Arial"
-    ctx.fillText('START', (ctxData.width /2)-(menuData.buttonWidth /2) + 12,(ctxData.height /2)-(menuData.buttonHeight /2) + 16)
+    ctx.font="32px Arial"
+    ctx.fillText('PRESS ENTER', (ctxData.width /2)-(menuData.buttonWidth /2) + 14,(ctxData.height /2)-(menuData.buttonHeight /2) + 22);
 }
 
 
 (function initApp(){
-    console.log('hello');
     defineCanvasStyle(c, ctxData.width , ctxData.height);
     drawMenu();
     gameEngine();
 })()
-console.log(window.innerWidth)
-console.log(window.innerHeight);
+
+document.addEventListener('keydown', function(e){
+    console.log(e.keyCode)
+    if(!engineData.gameStarted){
+        if(e.keyCode == 13){
+            engineData.activeFuncs.push(testFunc1);
+            engineData.activeFuncs.push(testFunc2);
+            engineData.gameStarted = true;
+            gameEngine();
+        }
+    }
+})
