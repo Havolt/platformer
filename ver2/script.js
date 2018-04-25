46ef5c5162b55fd2904caf7b0264ab086537107e
Mark Fitzpatrick <markfitz815@gmail.com>
2018-04-25 22:15:13 +0100

    created function to draw level

--- ver2/script.js
+++ ver2/script.js
@@ -13,18 +13,40 @@ let ctxData = {
     tileSize: 48
 };
 
+//Data for game
+let gameData = {
+    currLevel : []
+}
+
 
 //Sets the css of canvas
 function setCanvasStyle(cId, width, height){
-    console.log(cId)
     cId.setAttribute('width', width + 'px');
     cId.setAttribute('height', height + 'px');
 }
 
+function drawLevelLoop(level){
+    for(let i = 0; i < level.map.length; i++){
+        for(let j = 0; j < level.map[i].length; j++){
+            drawLevel(level.map[i][j], i, j);
+        }  
+    }
+}
+
+function drawLevel(tile, y, x){
+    if(tile == 0){
+        ctx.fillStyle='blue';
+    }else if(tile == 1){
+        ctx.fillStyle='green';
+    }
+    ctx.fillRect(x * ctxData.tileSize, y * ctxData.tileSize, ctxData.tileSize, ctxData.tileSize);
+}
+
 //Initialize startup functions
 function initApp(){
-    setCanvasStyle(gc, ctxData.width, ctxData.height)
-    console.log(levels);
+    setCanvasStyle(gc, ctxData.width, ctxData.height);
+    gameData.currLevel = levels.level1;
+    drawLevelLoop(gameData.currLevel);
 }
 
 
